d111ca276492f519980c3113944cebe591119a0f
bradmck <bradmck@gmail.com>
2023-04-17 08:19:13 -0400

    Another try

--- components/lovyangfx-display/lovyangfx.h
+++ components/lovyangfx-display/lovyangfx.h
@@ -32,7 +32,7 @@ class LovyanGFX :   public lgfx::LGFX_Device,
     LGFX_Sprite canvas;
     lgfx::LGFX_Device lcd;
       
-    LGFX(void)
+    LovyanGFX(void)
     {
         {                                      // バス制御の設定を行います。
             auto cfg = _bus_instance.config(); // バス設定用の構造体を取得します。
