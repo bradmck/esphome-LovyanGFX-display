1492d196d377f505f9bc75d07aa404613c3be458
bradmck <bradmck@gmail.com>
2023-04-22 07:41:18 -0400

    more hackery

--- components/lovyangfx-display/lovyangfx.h
+++ components/lovyangfx-display/lovyangfx.h
@@ -17,7 +17,7 @@ namespace lovyangfx {
 
 static const char *const TAG = "LovyanGFX";
 
-LovyanGFX lcd;
+//LovyanGFX lcd;
 LGFX_Sprite spr;
 
 class LovyanGFX :   public lgfx::LGFX_Device,
@@ -35,7 +35,7 @@ class LovyanGFX :   public lgfx::LGFX_Device,
     // LGFX_Sprite canvas;
     // lgfx::LGFX_Device lcd;
       
-    lgfx::LGFX_Device LovyanGFX(void)
+    LovyanGFX(void)
     {
         {                                      // バス制御の設定を行います。
             auto cfg = _bus_instance.config(); // バス設定用の構造体を取得します。
@@ -90,26 +90,21 @@ class LovyanGFX :   public lgfx::LGFX_Device,
 
         setPanel(&_panel_instance); // 使用するパネルをセットします。
     
-    { // バックライト制御の設定を行います。（必要なければ削除）
-      auto cfg = _light_instance.config();    // バックライト設定用の構造体を取得します。
+        { // バックライト制御の設定を行います。（必要なければ削除）
+        auto cfg = _light_instance.config();    // バックライト設定用の構造体を取得します。
 
-      cfg.pin_bl = 38;              // バックライトが接続されているピン番号
-      cfg.invert = false;           // バックライトの輝度を反転させる場合 true
-      cfg.freq   = 44100;           // バックライトのPWM周波数
-      cfg.pwm_channel = 7;          // 使用するPWMのチャンネル番号
+        cfg.pin_bl = 38;              // バックライトが接続されているピン番号
+        cfg.invert = false;           // バックライトの輝度を反転させる場合 true
+        cfg.freq   = 44100;           // バックライトのPWM周波数
+        cfg.pwm_channel = 7;          // 使用するPWMのチャンネル番号
 
-      _light_instance.config(cfg);
-      _panel_instance.setLight(&_light_instance);  // バックライトをパネルにセットします。
-    }
+        _light_instance.config(cfg);
+        _panel_instance.setLight(&_light_instance);  // バックライトをパネルにセットします。
+        }
     } 
-};
 
-class LGFXDisplay : public PollingComponent,
-                    public display::DisplayBuffer
-{
 
-    //LovyanGFX lcd;
-
+    LovyanGFX lcd;
     void setup() override {
         lcd.init();
         //lcd.setRotation(3);
@@ -160,7 +155,16 @@ class LGFXDisplay : public PollingComponent,
         ESP_LOGCONFIG(TAG, "  TFT Height: %d", lcd.height());
 
 
-    }
+    }    
+};
+
+class LGFXDisplay : public PollingComponent,
+                    public display::DisplayBuffer
+{
+
+    //LovyanGFX lcd;
+
+
 };
 
 }  // namespace lovyangfx
